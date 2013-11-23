48a1396a1268fecc151aef15732617d8831ae670
Adam <adam.gedney@gmail.com>
2013-11-23 12:48:49 -0500

    Services & request page(w/ form) coded

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <!-- Thanks for checking out my code!!
 
 
-	Copyright Adam Gedney 11/8/13
+	Copyright Adam Gedney 11/21/13
 	contact: adam.gedney@gmail.com
 	http://adamgedney.com
 
@@ -33,13 +33,12 @@
 	<link href="./fancyBox/helpers/jquery.fancybox-thumbs.css?v=2.0.5" rel="stylesheet" type="text/css" media="screen" />
 	<link rel="stylesheet" href="css/main.css" />
 
-
-
 	<!--[if lt IE 9]>
 	<script src="js/html5shiv.js"></script>
 	<![endif]-->
 </head>
 	<body>
+
 
 		<header>
 			<!-- <h1 class="hide-text">SG Internet Marketing</h1> -->
@@ -54,34 +53,143 @@
 			</nav>
 		</header>
 
+
+
+
+
 			<div class="container">
 				<div class="page landing">
-					<div class="featured-info">
-						<h1>SEO Services That Get Results</h1>
-							<p>We optimize your website to increase organic website traffic from all major search engines including Google, Yahoo and Bing.</p>
-								<button href="#">Request A Proposal</button>
-					</div><!-- /.featured-info-->
-					<img class="landing-img" src="/images/barchart.png" alt="SEO Bar Chart"/>
+					<div class="page-inner">
+						<div class="featured-info">
+							<h1>SEO Services That Get Results</h1>
+								<p>We optimize your website to increase organic website traffic from all major search engines including Google, Yahoo and Bing.</p>
+									<button href="#">Request A Proposal</button>
+						</div><!-- /.featured-info-->
+						<img class="landing-img" src="/images/barchart.png" alt="SEO Bar Chart"/>
+					</div><!-- /.page-inner-->
 				</div><!-- /.page-->
 
+
+
+
+
 				<div class="page">
-				
+					<div class="page-inner ">
+
+
+						<div class="box">
+							<h2>Website Analysis</h2>
+								<div class="box-inner">
+									<img src="/images/magnifyingglass.png" alt="service icon"/>
+								</div><!-- /.box-inner-->
+						</div><!-- /.box-->
+
+
+
+						<div class="box">
+							<h2>SEO Services</h2>
+								<div class="box-inner">
+									<img src="/images/monitor.png" alt="service icon"/>
+								</div><!-- /.box-inner-->
+						</div><!-- /.box-->
+
+
+
+						<div class="box">
+							<h2>Track Record</h2>
+								<div class="box-inner">
+									<img src="/images/linegraph.png" alt="service icon"/>
+								</div><!-- /.box-inner-->
+						</div><!-- /.box-->
+
+
+
+						<div class="span-columns">
+							<p><span class="red bold">You get what you pay for!</span> Don’t fall victim to the “cheap seo services” trap and risk hurting your website, your business, and your bottom line. Our SEO techniques are proven to stand the test of time. Just take a look at our track record. </p>
+						</div><!-- /.span-columns-->
+
+
+					</div><!-- /.page-inner-->
 				</div><!-- /.page-->
+
+
+
+
 
 				<div class="page request">
+					<div class="page-inner">
+						<div class="box features">
+							<h1>Request A Proposal</h1>
+								<h2>What you'll get in your custom proposal:</h2>
+
+								<ul>
+									<li>Keyword & competitor research</li>
+									<li>An analysis of your current website’s visibility</li>
+									<li>Search performance and profitability strategy detail</li>
+									<li>Pricing and package options</li>
+									<li>Budget based project milestone report</li>
+								</ul>
+						</div><!-- /.box-->
+
+						<div class="box">
+							<div class="form">
+								<form action="" method="post">
+									<label for="name">Name<span class="super red">*</span></label>
+									<input type="text" name="name" placeholder="Full Name"/>
+
+									<label for="email">Email<span class="super red">*</span></label>
+									<input type="email" name="email" placeholder="you@example.com"/>
+
+									<label for="website">Website<span class="super red">*</span></label>
+									<input type="url" name="website" placeholder="http://example.com"/>
+
+									<label for="phone">Phone<span class="super red">*</span></label>
+									<input type="tel" name="phone" placeholder="555-555-1212"/>
+
+									<label for="services">Services<span class="super red">*</span></label>
+									<div class="checkboxes">
+										
+										<input type="checkbox" name="services" value="SEO"/>
+										<p>SEO</p>
+										
+										<input type="checkbox" name="services" value="Web Design"/>
+										<p>Web Design</p>
+										
+										<input type="checkbox" name="services" value="Penalty Recovery"/>
+										<p>Penalty Recovery</p>
+									</div><!-- /.checkboxes-->
+
+									<input type="submit" value="Request A Proposal" />
+
+								</form>
+							</div><!-- /.form-->
+						</div><!-- /.box-->
 				
+					</div><!-- /.page-inner-->
 				</div><!-- /.page-->
+
+
+
+
 
 				<div class="page">
+					<div class="page-inner">
 				
+					</div><!-- /.page-inner-->
 				</div><!-- /.page-->
-				
 			</div><!-- /.container-->	
 
-		<footer class="snap">
+
+
+
+
+		<footer>
 			<img src="/images/logo.png" alt="SG Internet Marketing Logo"/>
 				<p>&copy;2013 SG Internet Marketing  |  <a href="mailto:sgedney@gmail.com?subject:SG Internet Marketing contact inquiry">Contact SG Internet Marketing</a></p>
 		</footer>
+
+
+
 
 		<!-- Scripts -->
 		<script src="js/jquery-1.10.2.min.js"></script>
