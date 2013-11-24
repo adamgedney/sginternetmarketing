6c20faaf58d59e073eb41be2743bc38fbfdd9d03
Adam <adam.gedney@gmail.com>
2013-11-24 17:39:57 -0500

    Built contact page. - getting tired now. Kinda hacky

--- controllers/mailer.php
+++ controllers/mailer.php
@@ -39,6 +39,41 @@ if(isset($_GET['action'])){
 		mail($to,$subject,$message,$header);
 		header('Location: /pages/thankyou.html');
 
+
+
+
+	//contact form handler
+	}if($_GET['action'] == "contact"){
+
+		$name = $_POST['name'];
+		$from = $_POST['email'];
+		$site = $_POST['website'];
+		$phone = $_POST['phone'];
+		$message = $_POST['message'];
+
+		//MAIL new subscriber info
+		$header  = 'MIME-Version: 1.0' . "\r\n";
+		$header .= "Reply-To: info@sgwebmarketing.com\r\n";
+		$header .= "Return-Path: info@sgwebmarketing.com\r\n";
+		$header .= 'From: Adam Gedney <info@sgwebmarketing.com>' . "\r\n";
+
+		$to = 'info@sgwebmarketing.com';
+		$subject = "New Contact Form Submission From SG Internet Marketing";
+
+		$message = "You have a new Contact inquiry. \r\n \r\n" . 
+		"Name: " . $name . " \r\n" . 
+		"Email: " . $from . " \r\n" .
+		"Website: " . $site . " \r\n" .  
+		"Phone: " . $phone . " \r\n" . 
+		"Message: " . $message . " \r\n \r\n" .
+		"Keep on Boogaloo! \r\n" .
+		"The SGIM Website Aura";
+		
+		//send email
+		// mail($to,$subject,$message,$header);
+		mail($to,$subject,$message,$header);
+		header('Location: /pages/thankyou.html');
+
 	}// /action
 }// /isset
 ?>
--- pages/contact.php
+++ pages/contact.php
@@ -1,1 +1,207 @@
-contact
+<?php require_once '../controllers/mailer.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<!-- Thanks for checking out my code!!
+
+
+	Copyright Adam Gedney 11/21/13
+	contact: adam.gedney@gmail.com
+	http://adamgedney.com
+
+"Good UI design is humble.” – Jono DiCarlo -->
+<head>
+	<title>SG Internet Marketing</title>
+
+	<!-- meta -->
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
+	<!-- favicon & humans.txt -->
+	<link type="text/plain" rel="author" href="/humans.txt" />
+	<link rel="shortcut icon" href="http://adamshields.com/favicon.ico" />
+	
+	<!-- Googel fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Raleway:400,300' rel='stylesheet' type='text/css'>
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="/css/animate.css" />
+	<link rel="stylesheet" href="/css/main.css" />
+	
+
+	<!--[if lt IE 9]>
+	<script src="js/html5shiv.js"></script>
+	<![endif]-->
+</head>
+	<body class="contact-page">
+
+
+		<header>
+			<!-- <h1 class="hide-text">SG Internet Marketing</h1> -->
+			<a href="/"><img src="/images/logo.png" alt="SG Internet Marketing Logo"/></a>
+
+			<nav>
+				<ul>
+					<li><a href="/pages/services.html">Services</a></li>
+					<li><a href="/pages/blog">Blog</a></li>
+					<li><a href="/pages/contact.php">Contact</a></li>
+				</ul>
+			</nav>
+		</header>
+
+
+
+
+
+			<div class="container">
+				<div class="page">
+					<div class="page-inner contact-wrapper">
+
+
+						<div class="box">
+							<div class="form">
+								<form action="?action=contact" method="post">
+									<label for="name">Name*</label>
+									<input type="text" name="name" placeholder="Full Name"/>
+
+									<label for="email">Email*</label>
+									<input type="email" name="email" placeholder="you@example.com"/>
+
+									<label for="website">Website*</label>
+									<input type="url" name="website" placeholder="http://example.com"/>
+
+									<label for="phone">Phone*</label>
+									<input type="tel" name="phone" placeholder="555-555-1212"/>
+
+									<textarea name="message"></textarea>
+
+									<input type="submit" value="Contact Us" />
+
+								</form>
+							</div><!-- /.form-->
+						</div><!-- /.box-->
+
+
+						<div class="box features">
+							<h1>Contact Us</h1>
+								<h2>Feel free... We don't bite</h2>
+
+								<h3><span class="bold">Address:</span></h3>
+									<p>PO Box 5053</p>
+									<p>Key West, FL 33045</p>
+
+									<h3><span class="bold">Phone:</span></h3>
+									<p>(845) 222-8612</p>
+
+									<h3><span class="bold">Email:</span></h3>
+									<p>info@sgwebmarketing.com</p></div>
+								
+						</div><!-- /.box-->
+
+
+				
+					</div><!-- /.page-inner-->
+				</div><!-- /.page-->
+
+
+
+
+
+				<div class="page how">
+					<div class="page-inner">
+						<h1>How We Do It</h1>
+
+						<p>
+						We only work with the highest quality Web Developers and SEO
+						Experts to ensure your website and business are handled with the
+						utmost care and professionalism. Our main focus is on <a target="_blank" href="/pages/services">SEO</a>, <a href="/pages/services">Web
+						Development</a> and <a target="_blank" href="/pages/services">Recovery Services</a> because it’s our speciality
+						and we happen to be pretty darn good at it. We are vigilant in
+						our continuing education and make sure we are ahead of the
+						competition in our market research, search engine algorithm
+						updates, latest technology and best practices. There is no guess
+						work, only results. We work with new businesses just starting out 
+						by building SEO optimized and user-friendly websites with
+						visually appealing and clean designs as well as existing
+						businesses that are looking to increase visibility online by
+						optimizing their existing websites. Whether you’ve been in
+						business for 10 years or 10 days, we can help you achieve the
+						online success you’re hoping for. Has your site received a manual
+						penalty by Google? No problem! We’ve helped numerous clients
+						reverse manual actions by Google and get their sites ranking
+						again. We can help you too. <a target="_blank" href="/pages/contact">Contact Us</a> for more information.
+						</p>
+
+						<div class="social">
+							<div class="box">
+								<div class="box-inner">
+									<a target="_blank" href="http://www.twitter.com/sginternetmktng">
+										<img src="/images/twitter.png" alt="twitter icon"/>
+										<h3>Follow Us</h3>
+									</a>
+								</div><!-- /.box-inner -->
+							</div><!-- /.box -->
+
+							<div class="box">
+								<div class="box-inner">
+									<a target="_blank" href="http://www.facebook.com/sginternetmarketing">
+										<img src="/images/facebook.png" alt="facebook icon"/>
+										<h3>Like Us</h3>
+									</a>
+								</div><!-- /.box-inner -->
+							</div><!-- /.box -->
+
+							<div class="box">
+								<div class="box-inner">
+									<a target="_blank" href="https://plus.google.com/117301156683295113742/about">
+										<img src="/images/google.png" alt="google icon"/>
+										<h3>Circle Us</h3>
+									</a>
+								</div><!-- /.box-inner -->
+							</div><!-- /.box -->
+
+							<div class="box">
+								<div class="box-inner">
+									<a target="_blank" href="/pages/rss.xml">
+										<img src="/images/rss.png" alt="rss icon"/>
+										<h3>Subscribe To Us</h3>
+									</a>
+								</div><!-- /.box-inner -->
+							</div><!-- /.box -->
+						</div><!-- /.social-->
+
+					</div><!-- /.page-inner-->
+				</div><!-- /.page-->
+			</div><!-- /.container-->	
+
+
+
+
+
+		<footer>
+			<div class="box">
+				<img src="/images/logo.png" alt="SG Internet Marketing Logo"/>
+			</div><!-- /.box -->
+
+			<div class="box">
+				<nav>
+					<ul>
+						<li><a href="/pages/services">Services</a></li>
+						<li><a href="/pages/services">Blog</a></li>
+						<li><a href="/pages/services">Contact</a></li>
+					</ul>
+				</nav>
+
+				<p>SG Internet Marketing, LLC &copy; 2013</p>
+			</div><!-- /.box -->
+		</footer>
+
+
+
+
+		<!-- Scripts -->
+		<script src="js/jquery-1.10.2.min.js"></script>
+		<script src="js/modernizr.custom.30046.js"></script>
+		<script src="js/main.js"></script>
+		
+	</body>
+</html>
